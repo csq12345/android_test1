b37ec2fc58b6a9b96ec6b40146a8799306dd21cc
csq12345 <aqing.123@163.com>
2015-03-13 18:16:08 +0800

    fggf

--- PAAD_Compass/src/com/example/paad_compass/CompassActivity.java
+++ PAAD_Compass/src/com/example/paad_compass/CompassActivity.java
@@ -2,39 +2,63 @@ package com.example.paad_compass;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
+public class CompassActivity extends Activity
+{
 
-public class CompassActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		Button button1 = (Button) findViewById(R.id.button1);
 
+		button1.setOnClickListener(new View.OnClickListener()
+		{
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+			@Override
+			public void onClick(View v)
+			{
+				try
+				{
+					CompassView cView = (CompassView) findViewById(R.id.compassView1);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    
-   
+					cView.setBearing(45);
+				} catch (Exception e)
+				{
+					Log.d("dddd", e.getMessage());
+				}
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings)
+		{
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
--- PAAD_Compass/src/com/example/paad_compass/CompassView.java
+++ PAAD_Compass/src/com/example/paad_compass/CompassView.java
@@ -3,15 +3,15 @@ package com.example.paad_compass;
 import android.R.integer;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
 
 public class CompassView extends View
 {
 
-	
-	
 	public CompassView(Context context)
 	{
 		super(context);
@@ -22,6 +22,7 @@ public class CompassView extends View
 	{
 		super(context);
 		InitcompassView();
+		
 	}
 
 	public CompassView(Context context, AttributeSet attrs, int defaultStyle)
@@ -29,8 +30,9 @@ public class CompassView extends View
 		super(context);
 		InitcompassView();
 	}
-///[ InitcompassView
-	
+
+	// /[ InitcompassView
+
 	Paint markerPaint;
 	Paint textPaint;
 	Paint circlePaint;
@@ -39,41 +41,144 @@ public class CompassView extends View
 	String southString;
 	String westString;
 	int textHeight;
-	
+
 	void InitcompassView()
 	{
 		setFocusable(true);
-		
-		
-		Resources r=this.getResources();
-		
-		
-		circlePaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+
+		Resources r = this.getResources();
+
+		circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		circlePaint.setColor(r.getColor(R.color.background_color));
 		circlePaint.setStrokeWidth(1);
 		circlePaint.setStyle(Paint.Style.FILL_AND_STROKE);
-		
-		 northString=r.getString(R.string.cardinal_north);
-		 eastString=r.getString(R.string.cardinal_east);
-		 southString=r.getString(R.string.cardinal_south);
-		 westString=r.getString(R.string.cardinal_west);
-		 
-		 
-		 
-		
+
+		northString = r.getString(R.string.cardinal_north);
+		eastString = r.getString(R.string.cardinal_east);
+		southString = r.getString(R.string.cardinal_south);
+		westString = r.getString(R.string.cardinal_west);
+
+		textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		textPaint.setColor(r.getColor(R.color.text_color));
+
+		textHeight = (int) textPaint.measureText("yY");
+
+		markerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		markerPaint.setColor(r.getColor(R.color.marker_color));
+
 	}
-///]
+
+	// /]
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
 	{
 		int measuredWidth = Measure(widthMeasureSpec);
 		int measuredHeight = Measure(heightMeasureSpec);
 
-		int d=Math.min(measuredWidth, measuredHeight);
-		
+		int d = Math.min(measuredWidth, measuredHeight);
+
 		setMeasuredDimension(measuredWidth, measuredHeight);
-		
-		//super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+		// super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+	String angle = "=";
+
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+		int mMeasuredWidth = getMeasuredWidth();
+		int mMeasuredHeight = getMeasuredHeight();
+
+		int px = mMeasuredWidth / 2;
+		int py = mMeasuredHeight / 2;
+
+		int radius = Math.min(px, py);
+
+		canvas.drawCircle(px, py, radius, circlePaint);
+		canvas.save();
+		canvas.rotate(-bearing, px, py);
+
+		int textWidth = (int) textPaint.measureText("W");
+
+		int cardinalX = px - textWidth / 2;
+		int cardinalY = py - radius + textHeight;
+
+		for (int i = 0; i < 24; i++)
+		{
+			canvas.drawLine(px, py - radius, px, py - radius + 10, markerPaint);
+
+			canvas.save();
+
+			canvas.translate(0, textWidth);
+
+			if (i % 6 == 0)
+			{
+				String dirString = "";
+				switch (i)
+				{
+					case 0:
+					{
+						dirString = northString;
+						int arrowY = 2 * textHeight;
+						canvas.drawLine(px, arrowY, px - 5, 3 * textHeight,
+								markerPaint);
+						canvas.drawLine(px, arrowY, px + 5, 3 * textHeight,
+								markerPaint);
+					}
+						break;
+					case 6:
+						dirString = eastString;
+						break;
+					case 12:
+						dirString = southString;
+						break;
+					case 18:
+						dirString = westString;
+						break;
+					default:
+						break;
+				}
+			} else if (i % 3 == 0)
+			{
+				String angleString = String.valueOf(i * 15);
+				float angleTextWidth = textPaint.measureText(angle);
+
+				int angleTextX = (int) (px - angleTextWidth / 2);
+				int angleTextY = py - radius + textHeight;
+
+				canvas.drawText(angleString, angleTextX, angleTextY, textPaint);
+			}
+
+			canvas.restore();
+			canvas.rotate(15, px, py);
+		}
+		canvas.restore();
+
+		// super.onDraw(canvas);
+	}
+
+	@Override
+	public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent event)
+	{
+
+		// return super.dispatchPopulateAccessibilityEvent(event);
+
+		if (isShown())
+		{
+			String bearingStr = String.valueOf(bearing);
+			if (bearingStr.length() > AccessibilityEvent.MAX_TEXT_LENGTH)
+			{
+				bearingStr = bearingStr.substring(0,
+						AccessibilityEvent.MAX_TEXT_LENGTH);
+			}
+			event.getText().add(bearingStr);
+			return true;
+
+		} else
+		{
+			return false;
+		}
 	}
 
 	int Measure(int measureSpec)
@@ -95,11 +200,9 @@ public class CompassView extends View
 		return result;
 	}
 
-	///[  Ù–‘
+	// /[  Ù–‘
 	float bearing;
-	
-	
-	
+
 	public float getBearing()
 	{
 		return bearing;
@@ -108,7 +211,8 @@ public class CompassView extends View
 	public void setBearing(float bearing)
 	{
 		this.bearing = bearing;
+		sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
 	}
 
-	///]
+	// /]
 }
