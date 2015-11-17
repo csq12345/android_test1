3048fe4cf480a4ed2b3f843924ff709610f63f5c
csq12345 <aqing.123@163.com>
2015-11-18 00:59:23 +0800

    c

--- MyAttendants/src/com/cw/myattendants/broadcastreceiver/ReceiverGPSLocationInfo.java
+++ MyAttendants/src/com/cw/myattendants/broadcastreceiver/ReceiverGPSLocationInfo.java
@@ -3,30 +3,48 @@ package com.cw.myattendants.broadcastreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 
 public class ReceiverGPSLocationInfo extends BroadcastReceiver
 {
-	Handler uiHandler;
-	//传入一个句柄
-	public ReceiverGPSLocationInfo(Handler uIHandler)
-	{
-		uiHandler = uIHandler;
-	}
+    Handler uiHandler;
 
-	@Override
-	public void onReceive(Context context, Intent intent)
-	{
-		// TODO Auto-generated method stub
-		if (uiHandler != null)
-		{
-			Message msg = uiHandler.obtainMessage();
+    //传入一个句柄
+    public ReceiverGPSLocationInfo(Handler uIHandler)
+    {
+        uiHandler = uIHandler;
+    }
 
-			msg.setData(intent.getBundleExtra("gpsbundle"));
-			
-			uiHandler.sendMessage(msg);
-		}
-	}
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        // TODO Auto-generated method stub
+        String s1 = intent.getStringExtra("timecount");
+
+        String sss = intent.getStringExtra("gpsbundle");
+        if (sss != null)
+        {
+            Log.d("ReceiverGPS", sss);
+        }
+
+        if (uiHandler != null)
+        {
+
+            Message msg = uiHandler.obtainMessage();
+            Bundle bb = new Bundle();
+            if (sss != null)
+            {
+                bb.putString("str", sss);
+            }
+            bb.putString("timecount", s1);
+            msg.setData(bb);
+            uiHandler.sendMessage(msg);
+        }
+
+
+    }
 
 }
